3c1f6581e8c817dadf946450ec273f64a189d41b
YantUgli <bryantnanur@gmail.com>
2024-07-12 21:27:46 +0700

    revisi devs

--- frontend-2/src/components/landing_page/devs.tsx
+++ frontend-2/src/components/landing_page/devs.tsx
@@ -26,8 +26,8 @@ const Devs = (props: Props) => {
           target="_blank"
           rel="noopener noreferrer"
           className={`flex flex-col items-center p-6 rounded-lg min-w-64 ${theme == "dark"
-              ? "bg-blue-400 bg-opacity-20"
-              : "bg-green-100 shadow-md"
+            ? "bg-blue-400 bg-opacity-20"
+            : "bg-green-100 shadow-md"
             } transform hover:scale-105 transition-transform`}
         >
           <Image
@@ -42,12 +42,32 @@ const Devs = (props: Props) => {
           <p className={` ${theme == "dark" ? "text-white" : "text-gray-600"}`}>PT Matra Hillindo</p>
         </Link>
         <Link
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`flex flex-col items-center p-6 rounded-lg min-w-64 ${theme == "dark"
+            ? "bg-white bg-opacity-20"
+            : "bg-yellow-200 shadow-md"
+            } transform hover:scale-105 transition-transform`}
+        >
+          <Image
+            src={Kebersamaan3}
+            alt="Developer 3"
+            width={80}
+            height={80}
+            className="rounded-full mb-4"
+          />
+          <h3 className={`font-semibold ${theme == "dark" ? "text-white" : "text-gray-800"}`}>Fadli</h3>
+          <p className={` ${theme == "dark" ? "text-white" : "text-gray-600"}`}>Front End Engineer</p>
+          <p className={` ${theme == "dark" ? "text-white" : "text-gray-600"}`}>PT Matra Hillindo</p>
+        </Link>
+        <Link
           href=""
           target="_blank"
           rel="noopener noreferrer"
           className={`flex flex-col items-center p-6 rounded-lg min-w-64 ${theme == "dark"
-              ? "bg-red-400 bg-opacity-20"
-              : "bg-blue-100 shadow-md"
+            ? "bg-red-400 bg-opacity-20"
+            : "bg-blue-100 shadow-md"
             } transform hover:scale-105 transition-transform`}
         >
           <Image
@@ -64,33 +84,14 @@ const Devs = (props: Props) => {
 
 
 
+
         <Link
           href="#"
           target="_blank"
           rel="noopener noreferrer"
           className={`flex flex-col items-center p-6 rounded-lg min-w-64 ${theme == "dark"
-              ? "bg-white bg-opacity-20"
-              : "bg-yellow-200 shadow-md"
-            } transform hover:scale-105 transition-transform`}
-        >
-          <Image
-            src={Kebersamaan3}
-            alt="Developer 3"
-            width={80}
-            height={80}
-            className="rounded-full mb-4"
-          />
-          <h3 className={`font-semibold ${theme == "dark" ? "text-white" : "text-gray-800"}`}>Fadli</h3>
-          <p className={` ${theme == "dark" ? "text-white" : "text-gray-600"}`}>Front End Engineer</p>
-          <p className={` ${theme == "dark" ? "text-white" : "text-gray-600"}`}>PT Matra Hillindo</p>
-        </Link>
-        <Link
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`flex flex-col items-center p-6 rounded-lg min-w-64 ${theme == "dark"
-              ? "bg-white bg-opacity-20"
-              : "bg-yellow-200 shadow-md"
+            ? "bg-white bg-opacity-20"
+            : "bg-yellow-200 shadow-md"
             } transform hover:scale-105 transition-transform`}
         >
           <Image
